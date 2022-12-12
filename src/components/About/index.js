d51938ae519edb225f6b12669d16e3acf8f28e7b
Your name here <brkmer@gmail.com>
2022-12-12 15:09:47 -0500

    test 2

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,6 +13,9 @@ function About() {
           My name is Brock Merritt, and I am a fresh full-stack web developer, with a focus on React.js. utilizing a background in communication to find and solve problems across the internet,as well a in within a groups cohesion. Recently certified in full stack development work from the UCF coding bootcamp, with knowledge and skills in JS,CSS, and various programming langauges used to manage databases. Specializing in back-end and overall project flow, as well as being able to present any new project/idea with less than a day of preparation, to any number of viewers. I worked with a team of 3 other people to create a single page MERN app that helps users relax, an out-of-the-box idea that shows the innovation and different thinking that my skills and mindset could bring to any web-development team. I have much experience with coaching and mentoring younger generations as well, being able to connect with people of different backgrounds and upbringings is a vital part of that role. I am excited to utilize my skill set in a progress driven industry.
           
         </div>
+        <div>
+          <a>test</a>
+        </div>
         <div className='links'>
           <a href="https://github.com/Brock-Merritt">
             <GitHubIcon style={{fontSize: 45 }}></GitHubIcon>
